bdeb2afb4d0dd0e7c83ba1ea140360d7272e3432
Jules Ruiz <jcrruiz06@gmail.com>
2026-03-02 16:01:53 -0600

    Fix HW6 misspelling token

--- puzzle.py
+++ puzzle.py
@@ -1,6 +1,6 @@
 puzzle_easy_key = 8983
 puzzle_easy_misspell = "gadqens"
 puzzle_key = 747585522
-puzzle_misspell = "mental"
+puzzle_misspell = "cirupmstances"
 cnet_id = "jcruiz"
 nonce = "79563878"
