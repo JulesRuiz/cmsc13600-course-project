f40972299283ca8cd281c399c49b8a325fe00c2b
Jules Ruiz <jcrruiz06@gmail.com>
2026-03-02 15:38:13 -0600

    HW6 puzzle key + misspell

--- puzzle.py
+++ puzzle.py
@@ -1,6 +1,6 @@
 puzzle_easy_key = 8983
 puzzle_easy_misspell = "gadqens"
-puzzle_key = 49677629
-puzzle_misspell = "tysxnt."
+puzzle_key = 747585522
+puzzle_misspell = "mental"
 cnet_id = "jcruiz"
 nonce = "79563878"
