99a4b7bfdce3c25af36c8dc69302435a6874601d
Jules Ruiz <jcrruiz06@gmail.com>
2026-03-06 20:31:39 -0600

    test

--- uncommondata/app/urls.py
+++ uncommondata/app/urls.py
@@ -5,11 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('editpage', views.editpage, name='editpage'),
     path('api/createUser/', views.createUser, name='createUser'),
-    path('api/upload/', views.uploads, name='upload'),
+    path('api/upload/', views.upload, name='upload'),
     path('api/dump-uploads/', views.dump_uploads, name='dump_uploads'),
     path('api/dump-data/', views.dump_data, name='dump_data'),
     path('api/knockknock/', views.knockknock, name='knockknock'),
-    path('uploads/', views.upload, name='uploads'),
-    path('api/download/<int:id>/', views.download, name='download'),
-    path('api/process/<int:id>/', views.process, name='process'),
+    path('uploads/', views.uploads, name='uploads'),
 ]
